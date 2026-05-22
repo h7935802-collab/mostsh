15d1fcc5e3c717f7ab58d28fb8e5788b72fd0ae9
h7935802-collab <h7935802@gmail.com>
2026-05-22 17:19:52 +0300

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'host' => 'localhost',
-    'port' => 3306,
-    'user' => 'root',
-    'password' => '',
-    'dbname' => 'emc_db'
+    'host' => 'dpg-d8745rsm0tmc739hfrdg-a',
+    'port' => 5432,
+    'user' => 'ems_user',
+    'password' => 'jQ04Dm39Hi3apwjo9bh0iwBbVTGFSePF',
+    'dbname' => 'ems_47x9'
 ];
